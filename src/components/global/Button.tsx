db2bff132a5dd4ce87a5a2f3cff438c0dd23da1c
Igbalajobi Zhikrullah <zalajobi@gmail.com>
2025-08-16 22:03:43 +0100

    setup firebase

--- src/components/global/Button.tsx
+++ src/components/global/Button.tsx
@@ -7,7 +7,7 @@ const CustomButton = ({
 }) => {
   return (
     <button
-      className={`bg-[#03034D] cursor-pointer text-white px-6 py-3 rounded-full font-medium hover:bg-[#03034D]/90 transition-colors`}
+      className={`bg-[#03034D] cursor-pointer text-white px-6 py-3 rounded-full font-medium hover:bg-[#03034D]/90 transition-colors ${className}`}
     >
       {buttonText}
     </button>
