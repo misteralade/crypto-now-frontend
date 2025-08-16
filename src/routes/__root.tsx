e94cdbed78a09bc4b919d1b2b7e57413ff2bde53
DevNifemi <abioseimran@gmail.com>
2025-08-16 18:36:11 +0100

    remove constant nav stylings

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,21 +1,12 @@
-import {Fragment} from "react";
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Fragment } from "react";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <Fragment>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to={"/about"} className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </Fragment>
   ),
-})
+});
