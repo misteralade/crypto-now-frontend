3b910b1b6e89134d6488f8eceaecf1071c0f4a8a
DevNifemi <abioseimran@gmail.com>
2025-08-16 18:36:27 +0100

    added all sections to index route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,36 @@
-import { createFileRoute } from '@tanstack/react-router'
-import {Button} from "../components/ui";
+import { createFileRoute } from "@tanstack/react-router";
+import Navbar from "../components/global/Navbar";
+import HeroSection from "../sections/homepage/HeroSection";
+import StepsSection from "../sections/homepage/StepsSection";
+import WhyCryptoNow from "../sections/homepage/WhyCryptoNow";
+import Footer from "../components/global/Footer";
+import CustomButton from "../components/global/Button";
+import AllInOne from "../sections/homepage/AllInOne";
+import Testimonials from "../sections/homepage/Testimonials";
+import InstantTradeSection from "../sections/homepage/InstantTradeSection";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-
-      <Button>HELLO WORLD</Button>
+    <div>
+      <Navbar />
+      <HeroSection />
+      <InstantTradeSection />
+      <StepsSection />
+      <WhyCryptoNow />
+      <AllInOne />
+      <Testimonials />
+      <div className="max-w-[960px] my-20 mx-auto text-center">
+        <div className="text-9xl font-semibold">Your crypto,</div>
+        <div className="text-9xl text-[#BDBDBD] font-semibold -mt-4 mb-6">
+          your way
+        </div>
+        <CustomButton buttonText="Buy & sell crypto now" />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
