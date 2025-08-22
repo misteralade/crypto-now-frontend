1ef056a74aae4ad286f95048b969f6f76f94ea47
DevNifemi <abioseimran@gmail.com>
2025-08-22 18:16:20 +0100

    rename login to sign up

--- src/routes/login.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import LoginPage from "../sections/auth/LoginPage";
-
-export const Route = createFileRoute("/login")({
-  component: LoginPage,
-});
