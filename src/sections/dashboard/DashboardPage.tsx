97e628570ee4634a276447724719dfaedbde211f
DevNifemi <abioseimran@gmail.com>
2025-09-28 16:01:59 +0100

    use beforeload prop instead of useeffect

--- src/sections/dashboard/DashboardPage.tsx
+++ src/sections/dashboard/DashboardPage.tsx
@@ -1,23 +1,11 @@
 import Navbar from "../../components/global/navbar/Navbar.tsx";
 import DashboardContent from "./DashboardContent.tsx";
-import {useEffect} from "react";
-import {useNavigate} from "@tanstack/react-router";
-import {LOCAL_STORAGE_KEYS} from "../../util/constants.ts";
 
 export default function DashboardPage() {
-    const isLoggedIn = localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN) !== null;
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if(!isLoggedIn){
-            navigate({to: '/sign-in'})
-        }
-    }, [isLoggedIn]);
-
-    return (
-        <div className={`space-y-10 md:space-y-20 pb-14`}>
-            <Navbar />
-            <DashboardContent />
-        </div>
-    )
+  return (
+    <div className={`space-y-10 md:space-y-20 pb-14`}>
+      <Navbar />
+      <DashboardContent />
+    </div>
+  );
 }
