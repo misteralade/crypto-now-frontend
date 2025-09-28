d248c955a26532c0dc84173d63953de20909a608
DevNifemi <abioseimran@gmail.com>
2025-09-28 15:50:28 +0100

    when logged in, user shouldnt be able to access auth pages

--- src/routes/reset-password.tsx
+++ src/routes/reset-password.tsx
@@ -1,8 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import ResetPasswordPage from "../sections/auth/forgot-password/ResetPasswordPage.tsx";
-
+import { LOCAL_STORAGE_KEYS } from "../util/constants";
 
 export const Route = createFileRoute("/reset-password")({
-    component: ResetPasswordPage,
+  beforeLoad: () => {
+    const accessToken = localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
+    if (accessToken) {
+      throw redirect({
+        to: "/dashboard",
+      });
+    }
+  },
+  component: ResetPasswordPage,
 });
-
--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -1,6 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import SignInPage from "../sections/auth/SignInPage";
+import { LOCAL_STORAGE_KEYS } from "../util/constants";
 
 export const Route = createFileRoute("/sign-in")({
+  beforeLoad: () => {
+    const accessToken = localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
+    if (accessToken) {
+      throw redirect({
+        to: "/dashboard",
+      });
+    }
+  },
   component: SignInPage,
 });
