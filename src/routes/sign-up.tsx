e89c7ef5d34c9732443361a14c0f2c2d183dfd4d
DevNifemi <abioseimran@gmail.com>
2025-09-28 15:50:28 +0100

    when logged in, user shouldnt be able to access auth pages

--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -1,6 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import SignUpPage from "../sections/auth/SignUpPage";
+import { LOCAL_STORAGE_KEYS } from "../util/constants";
 
 export const Route = createFileRoute("/sign-up")({
+  beforeLoad: () => {
+    const accessToken = localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
+    if (accessToken) {
+      throw redirect({
+        to: "/dashboard",
+      });
+    }
+  },
   component: SignUpPage,
 });
