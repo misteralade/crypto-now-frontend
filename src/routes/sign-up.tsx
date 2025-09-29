208434eabecc590c029b9e4ce93915cca2d14573
Igbalajobi Zhikrullah Oluwaranti <45386655+Zalajobi@users.noreply.github.com>
2025-09-29 16:24:04 +0100

    Revert "Feature/fix workflows"

--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -1,15 +1,6 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import SignUpPage from "../sections/auth/SignUpPage";
-import { LOCAL_STORAGE_KEYS } from "../util/constants";
 
 export const Route = createFileRoute("/sign-up")({
-  beforeLoad: () => {
-    const accessToken = localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
-    if (accessToken) {
-      throw redirect({
-        to: "/dashboard",
-      });
-    }
-  },
   component: SignUpPage,
 });
--- src/sections/dashboard/DashboardPage.tsx
+++ src/sections/dashboard/DashboardPage.tsx
@@ -1,11 +1,23 @@
 import Navbar from "../../components/global/navbar/Navbar.tsx";
 import DashboardContent from "./DashboardContent.tsx";
+import {useEffect} from "react";
+import {useNavigate} from "@tanstack/react-router";
+import {LOCAL_STORAGE_KEYS} from "../../util/constants.ts";
 
 export default function DashboardPage() {
-  return (
-    <div className={`space-y-10 md:space-y-20 pb-14`}>
-      <Navbar />
-      <DashboardContent />
-    </div>
-  );
+    const isLoggedIn = localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN) !== null;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!isLoggedIn){
+            navigate({to: '/sign-in'})
+        }
+    }, [isLoggedIn]);
+
+    return (
+        <div className={`space-y-10 md:space-y-20 pb-14`}>
+            <Navbar />
+            <DashboardContent />
+        </div>
+    )
 }
